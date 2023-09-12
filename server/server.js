115b382e2e7bd2ce3c21561a54e2416d608f9b50
trickerbaby <nnnanu2@gmail.com>
2023-09-12 21:49:49 +0530

    2148

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const openai = new OpenAI({apiKey: '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'});
+const openai = new OpenAI({apiKey: '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'});
 
 const password = "Navya#1427";
 const enpassword = encodeURIComponent("Navya#1427");
